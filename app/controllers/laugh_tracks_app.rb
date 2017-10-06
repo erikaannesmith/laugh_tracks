cb1ab6cd6adff3b21091b3400468f7038aa2a5a6
Erika Smith <erikaannesmith@gmail.com>
2017-10-06 10:59:08 -0600

    Iteration 1 & 3 functional, 2 & 5 began but not functional

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,9 @@
 class LaughTracksApp < Sinatra::Base
+
+  get '/comedians' do
+    @comedians = Comedian.all
+    @specials = Special.all
+    erb :index
+  end
+
 end
--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,3 +1,13 @@
 class Comedian < ActiveRecord::Base
 
+  def self.average_age
+    average(:age).to_i
+  end
+
+  def special
+    @specials = Special.all
+    require 'pry'; binding.pry
+    @specials.find_by(name: params[:name])
+  end
+
 end
